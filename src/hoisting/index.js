defb59c5e9270ec8c8f37b88a43abab2332a06c1
Ramiro Uffelmann <ramirouf@gmail.com>
2022-05-25 12:45:58 -0300

    New files

---
+++ src/hoisting/index.js
@@ -0,0 +1,18 @@
+a = 2;
+//var a ;
+console.log(a);
+
+console.log(a);
+var a = 2;
+
+dogName("Mora");
+function dogName(name) {
+  console.log(name);
+}
+dogName("Mora");
+
+//dogName("Mora");
+const dogName = (name) => {
+  console.log(name);
+};
+dogName("Mora");
